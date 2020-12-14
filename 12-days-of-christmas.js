04ffd9fe0aaddac99697eb652902df9e177affd6
RobColeman98 <Robcoleman98@gmail.com>
2020-12-14 17:12:40 -0600

    done with my practice drill

---
+++ 12-days-of-christmas.js
@@ -0,0 +1,64 @@
+  let christmasTime = () => {
+    setTimeout(() => {
+        console.log("Twelve drummers drumming!")
+    }, 2000)
+
+    setTimeout(() => {
+        console.log("Eleven pipers piping")
+    }, 4000)
+
+    setTimeout(() => {
+        console.log("Ten lords a-leaping")
+    }, 6000)
+
+    setTimeout(() => {
+        console.log("Nine ladies dancing")
+    }, 8000)
+
+    setTimeout(() => {
+        console.log("Eight maids a milking")
+    }, 10000)
+
+    setTimeout(() => {
+        console.log("Seven swans a-swimming")
+    }, 12000)
+
+    setTimeout(() => {
+        console.log("Six geese a-laying")
+    }, 14000)
+
+    setTimeout(() => {
+        console.log("Five golden")
+    },16000)
+    
+    setTimeout(() => {
+        console.log("Ringsssssssss!")
+    },17000)
+
+    setTimeout(() => {
+        console.log("Four Calling birds")
+    }, 20000)
+
+    setTimeout(() => {
+        console.log("Three French Hens")
+    }, 22000)
+
+    setTimeout(() => {
+        console.log("Two turtle doves")
+    }, 24000)
+
+    setTimeout(() => {
+        console.log("and")
+    }, 25000)
+
+    setTimeout(() => {
+        console.log("A partridge in a pear tree!")
+    }, 26000)
+
+    setTimeout(() => {
+        console.log("11 more days until Christmas")
+    }, 29000)
+}
+
+console.log("On the twelfth day of Christmas, my true love sent to me")
+christmasTime();
